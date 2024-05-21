0c2b4fefdbf4d2b06a65256bfc273a224e98832b
Gabriel Silva <gabriel.silva@pixieset.com>
2024-05-20 22:51:45 -0400

    plex ansible poc

--- vm-set/plex.sh
+++ vm-set/plex.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install -y ansible 
+# Define variables
+REPO_URL="https://github.com/gabepsilva/HomeLab4"
+BRANCH="Plex_Server"
+BASE_DIR="{$HOME}/.ansible/pull/plex.i.psilva.org"
+INVENTORY_PATH="${BASE_DIR}/inventory"
+PLAYBOOK_PATH="${BASE_DIR}/plex/local.yml"
+REQUIREMENTS_PATH="${BASE_DIR}/requirements.yml"
+
+# Pull the latest code from the repository
+git clone -b $BRANCH $REPO_URL $BASE_DIR
+
+# Install the required roles
+ansible-galaxy install -r $REQUIREMENTS_PATH
+
+# Run the ansible-pull command
+ansible-pull -U $REPO_URL -C $BRANCH -i $INVENTORY_PATH $PLAYBOOK_PATH

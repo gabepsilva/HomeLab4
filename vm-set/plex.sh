11b0acfff36147ac4e2f10791de08ecc4ecdf113
Gabriel Silva <gabriel.silva@pixieset.com>
2024-05-21 00:05:30 -0400

    plex ansible poc

--- vm-set/plex.sh
+++ vm-set/plex.sh
@@ -5,4 +5,6 @@ sudo apt-get install -y ansible
 
 ansible-galaxy install geerlingguy.docker
 
+ansible-pull -U https://github.com/gabepsilva/HomeLab4 -C Plex_Server -i ~/.ansible/pull/plex.i.psilva.org/inventory plex/main.yml
+
 
